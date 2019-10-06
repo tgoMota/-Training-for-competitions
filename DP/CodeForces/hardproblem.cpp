e9dc74fe75591570d1a51e332d34d6711e89a0ef
Thiago <tgomota1@gmail.com>
2019-10-06 03:39:53 -0300

    WR

--- DP/CodeForces/hardproblem.cpp
+++ DP/CodeForces/hardproblem.cpp
@@ -6,25 +6,25 @@ using namespace std;
 typedef long long ll;
 const int m = 1e5+7;
 int en[m], n;
-ll memo[3][m];
+ll memo[4][m];
 string v[m];
 
-ll dp(int ind,int goswap, string ant){ // goswap = swaped = ( 0 => NO , 1 => swap 1&n , 2 => swap 2&n-1 )
+ll dp(int ind,int goswap, string ant){ // goswap = swaped = ( 0 => NO , 1 => swap 1&n , 2 => swap 2&n-1, 3 => swap both )
     if(ind > n) return 0;
-    if(goswap && v[ind].size() == 1 || goswap == 2 && v[ind].size() <= 3) return oo;
+    if(goswap && v[ind].size() == 1 || goswap >= 2 && v[ind].size() <= 3) return oo;
     string newStr = v[ind];
     ll energy = 0LL;
     if(goswap){
         energy += en[ind];
         int len = newStr.size();
-        if(goswap == 1) swap(newStr[0], newStr[len-1]);
-        else swap(newStr[1], newStr[len-2]);
+        if(goswap == 1 || goswap == 3) swap(newStr[0], newStr[len-1]);
+        if(goswap == 2 || goswap == 3) swap(newStr[1], newStr[len-2]);
     }
     if(newStr < ant) return oo;
     ll& ans = memo[goswap][ind];
     if(ans != -1) return ans;
     ans = oo;
-    for(int i = 0; i <= 2 ; ++i) ans = min(ans,dp(ind+1,i,newStr)+energy);
+    for(int i = 0; i <= 3 ; ++i) ans = min(ans,dp(ind+1,i,newStr)+energy);
     return ans;
 }
 
@@ -33,7 +33,8 @@ int main(){
     for(int i = 1; i <= n ; ++i) cin >> en[i];
     for(int i = 1; i <= n ; ++i) cin >> v[i];
     memset(memo, -1, sizeof(memo));
-    ll ans = min({dp(1,0,""),dp(1,1,""),dp(1,2,"")});
+    ll ans = oo;
+    for(int i = 0; i <= 3 ; ++i) ans = min(ans,dp(1,i,""));
     cout << (ans < oo ? ans : -1) << endl;
     return 0;
 }
