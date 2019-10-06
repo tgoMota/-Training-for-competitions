c6312ea22a9021c33c82288ecc85df36f295ca58
Thiago <tgomota1@gmail.com>
2019-10-06 14:36:22 -0300

    xD

---
+++ Binary Search/UVa/12032.cpp
@@ -0,0 +1,30 @@
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3183
+//12032 - The Monkey and the Oiled Bamboo || UVa
+#include <bits/stdc++.h>
+using namespace std;
+const int m = 1e5+5;
+int rungs[m], t, n, clk;
+
+bool can(int mid){
+    for(int i = 1; i <= n ; ++i){
+        if(mid < rungs[i] - rungs[i-1]) return false;
+        if(mid == rungs[i]-rungs[i-1]) mid--;
+    }
+    return true;
+}
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> n;
+        for(int i = 1; i <= n ; ++i) cin >> rungs[i];
+        int lo = 0, hi = 1e7+10, mid;
+        while(lo < hi){
+            mid = (lo+hi)>>1;
+            if(can(mid)) hi = mid;
+            else lo = mid+1;
+        }
+        cout << "Case " << ++clk << ": " << lo << endl;
+    }
+
+    return 0;
+}
--- DP/CodeForces/hardproblem.cpp
+++ DP/CodeForces/hardproblem.cpp
@@ -1,5 +1,5 @@
 //https://codeforces.com/problemset/problem/706/C
-//"C. Hard problem"
+//"C. Hard problem" || CodeForces
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f3f3f3f3f
