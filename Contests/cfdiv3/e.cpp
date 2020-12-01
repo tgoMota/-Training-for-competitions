7f60bb29faa6fc430fe9a744776c64a45fe58cbd
Thiago Mota Carvalho <tgomota1@gmai.com>
2020-12-01 19:32:21 -0300

    up

--- Contests/cfdiv3/e.cpp
+++ Contests/cfdiv3/e.cpp
@@ -1,21 +1,92 @@
+//https://codeforces.com/contest/1454/problem/E
+//E. Number of Simple Paths
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
 #define ooLL 0x3f3f3f3f3f3f3f3f
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
-#define debug(x) cout << "DONT CRY " << x << endl
-#define debug2(x,y) cout << "DONT CRY {" << x << "," << y << "}\n"
+#define LOCAL
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
 const int mod = 1e9+7;
 typedef long long ll;
 typedef long double ld;
-typedef unsigned long long ull;
 typedef pair<int,int> ii;
- 
+
+vector<vector<int>> adj;
+vector<bool> cycle, vst;
+vector<int> parent, sz;
+int n;
+
+bool dfs(int u, int p){
+    vst[u] = true;
+    for(int& v : adj[u]){
+        if(v == p) continue;
+        if(vst[v]){
+            for(int t = u; t != v ; t = parent[t]){
+                cycle[t] = true;
+            }
+            cycle[v] = true;
+            return true;
+        }else{
+            parent[v] = u;
+            if(dfs(v, u)) return true;
+        }
+    }
+    return false;
+}
+
+void solve(int u, int p){
+    sz[u] = 1;
+    for(int& v : adj[u]){
+        if(cycle[v] || v == p) continue;
+        solve(v, u);
+        sz[u]+=sz[v];
+    }
+}
+
 int main(){
     int t;
     scanf("%d", &t);
     for(int ti = 1; ti <= t ; ++ti){
-        
+        int n;
+        scanf("%d", &n);
+        adj.assign(n, vector<int>());
+        parent.assign(n, -1);
+        sz.assign(n, 0);
+        cycle.assign(n, false);
+        vst.assign(n, false);
+        for(int i = 0; i < n ; ++i){
+            int a, b;
+            scanf("%d%d", &a, &b);
+            --a, --b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+
+        dfs(0, -1);
+        ll ans = 1LL * n*(n-1);
+        for(int i = 0; i < n ; ++i){
+            if(!cycle[i]) continue;
+            solve(i, -1);
+            ans -= 1LL*sz[i]*(sz[i]-1)/2;
+            trace(i, ans);
+        }
+        printf("%lld\n", ans);
     }
+
     return 0;
 }
--- Contests/cfdiv3/f.cpp
+++ Contests/cfdiv3/f.cpp
@@ -1,21 +1,103 @@
+//https://codeforces.com/contest/1454/problem/F
+//F. Array Partition
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
 #define ooLL 0x3f3f3f3f3f3f3f3f
 #define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
-#define debug(x) cout << "DONT CRY " << x << endl
-#define debug2(x,y) cout << "DONT CRY {" << x << "," << y << "}\n"
+#define LOCAL
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
 const int mod = 1e9+7;
 typedef long long ll;
 typedef long double ld;
-typedef unsigned long long ull;
 typedef pair<int,int> ii;
- 
+
+vector<int> v, pref, suf;
+
+struct SegMin{
+   vector<int> t;
+   void build(int i, int l, int r){
+     if(l == r) t[i] = v[l];
+     else{
+        int m = l + (r-l)/2;
+        build(2*i, l, m);
+        build(2*i+1, m+1, r);
+        t[i] = min(t[2*i], t[2*i+1]);
+     }
+   }
+
+   int query(int i, int l, int r, int ql, int qr){
+     if(ql <= l && qr >= r) return t[i];
+     if(qr < l || ql > r) return oo;
+     int m = l + (r-l)/2;
+     return min(query(2*i, l, m, ql, qr), query(2*i+1, m+1, r, ql, qr));
+   }
+    
+   void init(const int n){
+     t.assign(4*n+20, oo);
+     build(1, 0, (int)v.size()-1);
+   }
+};
+
 int main(){
     int t;
     scanf("%d", &t);
+    SegMin sgb;
     for(int ti = 1; ti <= t ; ++ti){
-        
+        int n;
+        scanf("%d", &n);
+        v.clear();
+        pref.clear();
+        v.resize(n);
+        for(int i = 0; i < n ; ++i) {
+          scanf("%d", &v[i]);
+          if(!i) pref.push_back(v[i]);
+          else pref.push_back(max(pref.back(), v[i]));
+        }
+        suf.assign(n, 0);
+        for(int i = n-1; i >= 0 ; i--){
+          if(i == n-1) suf[i] = v[i];
+          else suf[i] = max(v[i], suf[i+1]);
+        }
+
+        SegMin sg;
+        sg.init(n);
+        bool ok = false;
+        for(int i = n-1; i >= 2 && !ok ; i--){
+          int val = suf[i];
+          int begin = (int)(lower_bound(pref.begin(), pref.end(), val) - pref.begin());
+          int end = (int)(upper_bound(pref.begin(), pref.end(), val) - pref.begin());
+          int lo = begin+1, hi = end, mid;
+          while(lo <= hi){
+            mid = lo + (hi-lo)/2;
+            int q = sg.query(1,0,n-1,mid,i-1);
+            if(q == val){
+                if(i > mid && sg.query(1, 0, n-1, mid, i-1) == val){
+                  printf("YES\n");
+                  printf("%d %d %d\n", mid, i-mid, n-i);
+                  ok = true;
+                  break;
+              }
+            }
+            if(q < val) lo = mid+1;
+            else hi = mid-1;
+          }
+        }
+        if(!ok) printf("NO\n");
     }
     return 0;
 }
