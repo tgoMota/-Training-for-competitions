5e64839491a3b6b16136d0bbf62ee8bed52d6bdd
= <tgomota1@gmail.com>
2021-01-26 20:10:58 -0300

    add solution uri 1057

--- URI Solutions/1057.cpp
+++ URI Solutions/1057.cpp
@@ -2,28 +2,6 @@
 //URI 1057 - Chegando Junto
 #include <bits/stdc++.h>
 using namespace std;
-#define oo 0x3f3f3f3f
-#define ooLL 0x3f3f3f3f3f3f3f3f
-#define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
-#define LOCAL
-#ifdef LOCAL
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define trace(...) 42
-#endif
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-  cerr << name << ": " << arg1 << endl;
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-  const char* comma = strchr(names + 1, ',');
-  cerr.write(names, comma - names) << ": " << arg1 << " |";
-  __f(comma + 1, args...);
-}
-const int mod = 1e9+7;
-typedef long long ll;
-typedef long double ld;
 typedef pair<int,int> ii;
 //CHECK THE LIMITS, PLEASE
 int N;
@@ -78,7 +56,6 @@ int main(){
           vst.insert(v);
           for(int i = 0; i < 4 ; ++i){
             vector<ii> new_pos;
-            set<ii> test;
             set<pair<int,ii>> mustStay;
             set<pair<int,ii>> cango;
             for(int robot = 0; robot < 3 ; ++robot){
