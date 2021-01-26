5ef2981a0bb3029950032d560f0f2267a7110b62
= <tgomota1@gmail.com>
2021-01-26 17:27:11 -0300

    studying maxflow on UVA

--- UVA Solutions/a.cpp
+++ UVA Solutions/a.cpp
@@ -1,49 +1,64 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=761
+//UVA 820 - Internet Bandwidth
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
-#define ooLL 0x3f3f3f3f3f3f3f3f
-#define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
-#define LOCAL
-#ifdef LOCAL
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define trace(...) 42
-#endif
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-  cerr << name << ": " << arg1 << endl;
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-  const char* comma = strchr(names + 1, ',');
-  cerr.write(names, comma - names) << ": " << arg1 << " |";
-  __f(comma + 1, args...);
-}
-const int mod = 1e9+7;
-typedef long long ll;
-typedef long double ld;
-typedef pair<int,int> ii;
 //CHECK THE LIMITS, PLEASE
 const int MX = 105;
-int adj[MX][MX], res[MX][MX], N, S, T, C, parent[MX];
+int res[MX][MX], N, S, T, C, parent[MX];
 
-void maxflow(){
-  while(bfs()){
+int augmentPath(int v, int minflow){
+  if(parent[v] == -1) return minflow;
+  minflow = augmentPath(parent[v], min(minflow, res[parent[v]][v]));
+  res[parent[v]][v] -= minflow;
+  res[v][parent[v]] += minflow;
+  return minflow;
+}
 
+bool bfs(){
+  queue<int> q;
+  bool vst[MX];
+  memset(vst, false, sizeof(vst));
+  memset(parent, -1, sizeof(parent));
+  q.push(S);
+  vst[S] = true;
+  while(!q.empty()){
+    int v = q.front();
+    q.pop();
+    for(int i = 1; i <= N ; ++i){
+      if(!vst[i] && res[v][i] > 0){
+        q.push(i);
+        parent[i] = v;
+        vst[i] = true;
+      }
+    }
   }
+  return vst[T];
+}
+
+int maxflow(){
+  int maxflow = 0;
+  while(bfs()){
+    int flow = augmentPath(T, oo);
+    maxflow += flow;
+  }
+  return maxflow;
 }
 
 int main(){
+    int ti = 0;
     while(scanf("%d", &N) && N){
+      memset(res, 0, sizeof(res));
       scanf("%d%d%d", &S, &T, &C);
       for(int i = 0; i < C ; ++i){
         int a, b, c;
         scanf("%d%d%d", &a, &b, &c);
-        adj[a][b] = c;
-        res[a][b] = c;
+        res[a][b] += c;
+        res[b][a] += c;
       }
 
-      maxflow();
+      printf("Network %d\n",++ti);
+      printf("The bandwidth is %d.\n\n", maxflow());
     }
     return 0;
 }
