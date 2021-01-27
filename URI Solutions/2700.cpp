e1cf8ce35c09647051d3e89de1b5a35c8697a4df
= <tgomota1@gmail.com>
2021-01-27 03:09:37 -0300

    solved uri 2700 - Angariando fundos

--- URI Solutions/2700.cpp
+++ URI Solutions/2700.cpp
@@ -1,34 +1,100 @@
-// https://www.urionlinejudge.com.br/judge/pt/problems/view/2700
-// URI 2700 - Angariando Fundos
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/2700
+//URI 2700 - Angariando Fundos
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
-typedef long long ll;
-const int M = 100005;
-int t[M<<2];
-map<pair<int,int>, ll> compress;
-void build(int i, int l, int r, vector<int>& vet){
-    if(l == r) t[i] = vet[l];
-
+#define ooLL 0x3f3f3f3f3f3f3f3f
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
+#define LOCAL
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
 }
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+const int mod = 1e9+7;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+//CHECK THE LIMITS, PLEASE
+int n;
+vector<pair<ll,ii>> v;
 
+struct Seg{
+  vector<ll> t;
+  Seg(const int n){
+    t.assign(4*n, 0);
+  }
+
+  void update(int i, int l, int r, int p, ll x){
+        if(l > r) return;
+        if(l == r){
+            t[i] = max(t[i], x);
+            return;
+        }
+        int m = l + (r-l)/2;
+        if(p <= m) update(2*i, l, m, p, x);
+        else update(2*i+1, m+1, r, p, x);
+        t[i] = max(t[2*i], t[2*i+1]);
+    }
+
+    ll query(int i, int l, int r, int ql, int qr){
+        if(ql <= l && qr >= r) return t[i];
+        if(ql > r || qr < l) return 0;
+        int m = l + (r-l)/2;
+        return max(query(2*i, l, m, ql, qr), query(2*i+1, m+1, r, ql, qr));
+    }
+
+};
 
 int main(){
-    // int N;
-    // scanf("%d", &N);
-    // vector<int> a(N), b(N), c(N), auxa(N), auxb(N);
-    // for(int i = 0; i < N ; ++i){
-    //     scanf("%d%d%d", &a[i], &b[i], &c[i]);
-    //     auxa[i] = a[i];
-    //     auxb[i] = b[i];
-    // }
+    scanf("%d", &n);
+    v.resize(n);
+    map<ii,ll> compressed;
+    set<int> onlyf;
+    for(pair<ll,ii> &x : v) {
+      scanf("%d%d%lld", &x.second.first, &x.second.second, &x.first);
+      auto it = compressed.find(x.second);
+      onlyf.insert(x.second.second);
+      if(it == compressed.end()) compressed[x.second] = x.first;
+      else it->second += x.first;
+    }
+    unordered_map<int,int> countf;
+    int count = 0;
+    for(int x : onlyf){
+      countf[x] = count++;
+    }
+    vector<pair<ll,ii>> new_v;
+    int idx = 0;
+    for(auto x : compressed) {
+      pair<ii, ll> y = x;
+      y.first.second = countf[y.first.second];
+      new_v.push_back({y.second, y.first});
+    }
+    const int N = (int)new_v.size();
+    sort(new_v.begin(), new_v.end(), [](pair<ll,ii>& a, pair<ll,ii>& b){
+      if(a.second.first != b.second.first) return a.second.first < b.second.first;
+      return a.second.second > b.second.second;
+    });
 
-    // sort(auxa.begin(), auxa.end());
-    // sort(auxb.begin(), auxb.end());
+    Seg seg(N);
+    for(auto x : new_v){
+      ll before = seg.query(1, 0, N-1, 0, x.second.second-1);
+     // trace(x.first);
+      seg.update(1, 0, N-1, x.second.second, before+x.first);
+    }
 
+    ll ans = seg.query(1, 0, N-1, 0, N-1);
 
-
-    
-
+    printf("%lld\n", ans);
     return 0;
 }
--- URI Solutions/a.cpp
+++ URI Solutions/a.cpp
@@ -1,5 +1,5 @@
-//https://www.urionlinejudge.com.br/judge/pt/problems/view/1057
-//URI 1057 - Chegando Junto
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/2700
+//URI 2700 - Angariando Fundos
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
@@ -26,113 +26,75 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int,int> ii;
 //CHECK THE LIMITS, PLEASE
-int N;
-vector<string> grid;
-vector<ii> robots, targets;
-multiset<vector<ii>> vst;
-int dx[] = {1,-1,0,0}, dy[] = {0,0,1,-1};
+int n;
+vector<pair<ll,ii>> v;
 
-bool allReached(vector<ii>& v){
-  for(ii robot : v){
-    bool found = false;
-    for(ii target : targets){
-      if(robot == target) found = true;
-    }
-    if(!found) return false;
+struct Seg{
+  vector<ll> t;
+  Seg(const int n){
+    t.assign(4*n, 0);
   }
-  return true;
-}
+
+  void update(int i, int l, int r, int p, ll x){
+        if(l > r) return;
+        if(l == r){
+            t[i] = max(t[i], x);
+            return;
+        }
+        int m = l + (r-l)/2;
+        if(p <= m) update(2*i, l, m, p, x);
+        else update(2*i+1, m+1, r, p, x);
+        t[i] = max(t[2*i], t[2*i+1]);
+    }
+
+    ll query(int i, int l, int r, int ql, int qr){
+        if(ql <= l && qr >= r) return t[i];
+        if(ql > r || qr < l) return 0;
+        int m = l + (r-l)/2;
+        return max(query(2*i, l, m, ql, qr), query(2*i+1, m+1, r, ql, qr));
+    }
+
+};
 
 int main(){
-    int t;
-    scanf("%d", &t);
-    for(int ti = 1; ti <= t ; ++ti){
-        scanf("%d", &N);
-        grid.resize(N);
-        robots.clear();
-        targets.clear();
-        for(int i = 0; i < N ; ++i){
-          cin >> grid[i];
-          for(int j = 0; j < N ; ++j){
-            if(grid[i][j] == 'X') targets.push_back({i,j});
-            else if(isalpha(grid[i][j])) {
-              robots.push_back({i,j});
-            }
-          }
-        }
-
-        queue<pair<int,vector<ii>>> q;
-        q.push({0,robots});
-        int ans = -1;
-        vst.clear();
-        while(!q.empty()){
-          auto all = q.front();
-          q.pop();
-          vector<ii> v = all.second;
-
-          if((int)vst.count(v)) continue;
-          if(allReached(v)){
-            ans = all.first;
-            break;
-          }
-          vst.insert(v);
-          for(int i = 0; i < 4 ; ++i){
-            vector<ii> new_pos;
-            set<ii> test;
-            set<pair<int,ii>> mustStay;
-            set<pair<int,ii>> cango;
-            for(int robot = 0; robot < 3 ; ++robot){
-                ii pos = v[robot];
-                int new_x = dx[i] + pos.first;
-                int new_y = dy[i] + pos.second;
-                if(new_x < 0 || new_x >= N || new_y < 0 || new_y >= N || grid[new_x][new_y] == '#'){ //invalid position
-                  //new_pos.push_back(pos); //continue on that old position;
-                  mustStay.insert({robot,pos});
-                }else{
-                  cango.insert({robot, {new_x, new_y}});
-                  //new_pos.push_back({new_x, new_y});
-                }
-            }
-
-            for(auto x : cango){
-              ii pos1 = x.second;
-              bool toexit = false;
-              for(auto y : mustStay){
-                ii pos2 = y.second;
-                if(pos1 == pos2){
-                  ii new_pos1 = {pos1.first-dx[i], pos1.second-dy[i]};
-                  mustStay.insert({x.first, new_pos1});
-                  cango.erase(cango.find(x));
-                  toexit = true;
-                  break;
-                }
-              }
-              if(toexit) break;
-            }
-
-            for(auto x : cango){
-              ii pos1 = x.second;
-              bool toexit = false;
-              for(auto y : mustStay){
-                ii pos2 = y.second;
-                if(pos1 == pos2){
-                  ii new_pos1 = {pos1.first-dx[i], pos1.second-dy[i]};
-                  mustStay.insert({x.first, new_pos1});
-                  toexit = true;
-                  break;
-                }
-              }
-              if(toexit) break;
-              else mustStay.insert(x);
-            }
-            assert((int)mustStay.size() == 3);
-            for(auto x : mustStay) new_pos.push_back(x.second);
-            q.push({all.first+1,new_pos});
-          }
-        }
-        printf("Case %d: ", ti);
-        if(ans == -1) printf("trapped\n");
-        else printf("%d\n", ans);
+    scanf("%d", &n);
+    v.resize(n);
+    map<ii,ll> compressed;
+    set<int> onlyf;
+    for(pair<ll,ii> &x : v) {
+      scanf("%d%d%lld", &x.second.first, &x.second.second, &x.first);
+      auto it = compressed.find(x.second);
+      onlyf.insert(x.second.second);
+      if(it == compressed.end()) compressed[x.second] = x.first;
+      else it->second += x.first;
     }
+    unordered_map<int,int> countf;
+    int count = 0;
+    for(int x : onlyf){
+      countf[x] = count++;
+    }
+    vector<pair<ll,ii>> new_v;
+    int idx = 0;
+    for(auto x : compressed) {
+      pair<ii, ll> y = x;
+      y.first.second = countf[y.first.second];
+      new_v.push_back({y.second, y.first});
+    }
+    const int N = (int)new_v.size();
+    sort(new_v.begin(), new_v.end(), [](pair<ll,ii>& a, pair<ll,ii>& b){
+      if(a.second.first != b.second.first) return a.second.first < b.second.first;
+      return a.second.second > b.second.second;
+    });
+
+    Seg seg(N);
+    for(auto x : new_v){
+      ll before = seg.query(1, 0, N-1, 0, x.second.second-1);
+     // trace(x.first);
+      seg.update(1, 0, N-1, x.second.second, before+x.first);
+    }
+
+    ll ans = seg.query(1, 0, N-1, 0, N-1);
+
+    printf("%lld\n", ans);
     return 0;
 }
