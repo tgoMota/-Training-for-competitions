41a3602e912b9090d9480773d1a5574ccf7a7f15
= <tgomota1@gmail.com>
2021-02-11 21:08:51 -0300

    solving sobre random problems on cf

---
+++ Codeforces Solutions/f.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define oo 0x3f3f3f3f
+#define ooLL 0x3f3f3f3f3f3f3f3f
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(0)
+#define LOCAL
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+const int mod = 1e9+7;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+//CHECK THE CONSTRAINTS, PLEASE
+int main(){
+    int n, k;
+    scanf("%d%d", &n, &k);
+    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+    for(int i = 1; i <= n ; ++i){
+      for(int j = 0; j <= n-k ; ++j){
+        if(j == 0) dp[i][j] = n-1;
+      }
+    }
+    return 0;
+}
