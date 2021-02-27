85e9d4169604f7a3939921d9333dcc8a710b8fa1
= <tgomota1@gmail.com>
2021-02-27 19:02:16 -0300

    upsolving

--- URI Solutions/a.cpp
+++ URI Solutions/a.cpp
@@ -1,5 +1,3 @@
-//https://www.urionlinejudge.com.br/judge/pt/problems/view/3082
-//URI 3082 - Matador De Onça Mutante
 #include <bits/stdc++.h>
 using namespace std;
 #define oo 0x3f3f3f3f
@@ -26,124 +24,122 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int,int> ii;
 //CHECK THE CONSTRAINTS, PLEASE
-int N, K;
-vector<int> ans;
-struct CentroidDec{
-  int N;
-  vector<int> lv, size, sub_tree, dist_cent, dist_sub;
-  vector<bool> isCentroid;
-  vector<vector<int>> tree;
-  CentroidDec(){}
-  CentroidDec(const int n){
-    N = n;
-    size.assign(n+1, 0);
-    isCentroid.assign(n+1, false);
-    tree.assign(n+1, vector<int>());
-    lv.assign(n+1, 0);
-  }
-
-  void add_edge(int u, int v){
-    tree[u].push_back(v);
-    tree[v].push_back(u);
-  }
-
-  void dfs(int v, int p){
-    size[v] = 1;
-    for(int x : tree[v]){
-      if(x != p && !isCentroid[x]){
-        dfs(x, v);
-        size[v] += size[x];
-      }
-    }
-  }
-
-  void dfs1(int v, int p){
-    size[v] = 1;
-    dist_cent.push_back(lv[v]);
-    for(int x : tree[v]){
-      if(x != p && !isCentroid[x]){
-        lv[x] = lv[v] + 1;
-        dfs1(x,v);
-        size[v] += size[x];
-      }
-    }
-  }
-
-  void dfs2(int v, int p){
-    sub_tree.push_back(v);
-    size[v] = 1;
-    dist_sub.push_back(lv[v]);
-    for(int x : tree[v]){
-      if(x != p && !isCentroid[x]){
-        lv[x] = lv[v] + 1;
-        dfs2(x,v);
-        size[v] += size[x];
-      }
-    }
-  }
-
-  int getCentroid(int v, int p, const int n){
-    bool is_centroid = true;
-    int heaviest_child = 0;
-    for(int x : tree[v]){
-      if(x != p && !isCentroid[x]){
-        if(2*size[x] > n) is_centroid = false;
-        if(heaviest_child == 0 || size[heaviest_child] < size[x]) heaviest_child = x;
-      }
+vector<set<int>> adj;
+vector<unordered_map<int,bool>> new_adj;
+vector<vector<int>> scc;
+vector<int> id_component;
+int n, m;
+vector<bool> vst;
+vector<int> dfs_num, dfs_low, st;
+int lastVisited;
+void init(const int n){
+    dfs_num.assign(n, 0);
+    dfs_low.assign(n, 0);
+    vst.assign(n, false);
+    st.clear();
+}
+void tarjanSCC(int v, int& dfsCounter){
+    dfs_num[v] = dfs_low[v] = ++dfsCounter;
+    vst[v] = true;
+    st.push_back(v);
+    for(int u : adj[v]){
+        if(!dfs_num[u]) tarjanSCC(u, dfsCounter);
+        if(vst[u]) dfs_low[v] = min(dfs_low[v], dfs_low[u]);
     }
 
-    if(is_centroid && 2*size[v] >= n) return v;
-    return getCentroid(heaviest_child, v, n);
-  }
+    if(dfs_low[v] == dfs_num[v]){
+        vector<int> new_scc;
+        int idx = (int)scc.size();
+        while(1){
+            int t = st.back();
+            st.pop_back();
+            id_component[t] = idx;
+            vst[t] = false;
+            new_scc.push_back(t);
+            if(t == v) break;
+        }
+        scc.push_back(new_scc);
+    }
+}
 
-  int getCentroid(int v){
-    dfs(v, -1);
-    int centroid = getCentroid(v, -1, size[v]);
-    isCentroid[centroid] = true;
-    return centroid;
-  }
+// void print(){
+//   for(int i = 0; i < (int)scc.size() ; ++i){
+//     cout << i << ": ";
+//     for(auto u : new_adj[i]){
+//       int x = u.first;
+//       cout << x << " ";
+//     }
+//     cout << '\n';
+//   }
+// }
 
-  void decompose(int v){
-    int cent_root = getCentroid(v);
-    dist_cent.clear();
-    lv[cent_root] = 0;
-    dfs1(cent_root, -1);
-    sort(dist_cent.begin(), dist_cent.end());
-    int d = upper_bound(dist_cent.begin(), dist_cent.end(), K) -  dist_cent.begin();
-    ans[cent_root] += d;
-    for(int x : tree[cent_root]){
-      if(!isCentroid[x]){
-        sub_tree.clear();
-        dist_sub.clear();
-        lv[x] = 1;
-        dfs2(x, cent_root);
-        sort(dist_sub.begin(), dist_sub.end());
+// void printSCC(){
+//     trace("SCC");
+//     int idx = 0;
+//     for(vector<int>& scc : scc){
+//         cout << idx++ << ": ";
+//         for(int i = 0; i < (int)scc.size() ; ++i){
+//             if(i) printf(" ");
+//             printf("%d", scc[i]);
+//         }
+//         printf("\n");
+//     }
+// }
 
-        for(int node : sub_tree){
-          int add = upper_bound(dist_cent.begin(), dist_cent.end(), K-lv[node])-dist_cent.begin();
-          int repeated = upper_bound(dist_sub.begin(), dist_sub.end(), K-lv[node])-dist_sub.begin();
-          ans[node] += add - repeated;
+int main(){
+    fastio();
+    cin >> n >> m;
+    adj.assign(n+1, set<int>());
+    id_component.assign(n+1, -1);
+    new_adj.assign(n+1, unordered_map<int,bool>());
+    for(int i = 0; i < m ; ++i){
+      int a, b;
+      cin >> a >> b;
+      adj[a].insert(b);
+    }
+    init(n+1);
+    for(int i = 1, dfsCounter = 0; i <= n ; ++i) {
+        if(!dfs_num[i]) tarjanSCC(i, dfsCounter);
+    }
+    const int N = (int)scc.size();
+    vector<int> in_deg(N+1, 0), deg(N+1, 0);
+    for(int i = 1; i <= n ; ++i){
+      for(int x : adj[i]){
+        if(id_component[i] != id_component[x]){
+          if(new_adj[id_component[i]][id_component[x]]) continue;
+          new_adj[id_component[i]][id_component[x]] = true;
+          in_deg[id_component[x]]++;
+          deg[id_component[x]]++;
+          deg[id_component[i]]++;
         }
       }
     }
+    queue<int> q;
+    int root = 0;
+    for(int i = 0; i < N ; ++i){
+      if(in_deg[i] == 0){
+        q.push(i);
+      }
+    }
+    vector<int> topo;
+    int cnt = 0;
+    bool ok = true;
+    while(!q.empty()){
+      int v = q.front();
+      q.pop();
+      topo.push_back(v);
+      ++cnt;
+      if(cnt == 1 || cnt == N) ok &= (deg[v] == 1);
+      else ok &= (deg[v] == 2);
+      if(!ok || cnt == N) break;
+      for(auto u : new_adj[v]){
+        int x = u.first;
+        if(--in_deg[x] == 0) q.push(x);
+      }
+    }
 
-    for(int x : tree[cent_root]){
-      if(!isCentroid[x]) decompose(x);
-    }
-  }
-};
-int main(){
-    fastio();
-    cin >> N >> K;
-    CentroidDec cd(N);
-    for(int i = 0; i < N-1 ; ++i){
-      int a, b;
-      cin >> a >> b;
-      cd.add_edge(a,b);
-    }
-    ans.assign(N+1, 0);
-    cd.decompose(1);
-    for(int i = 1; i <= N ; ++i) cout << ans[i] << '\n';
+    if(ok && cnt == N) cout << "Bolada\n";
+    else cout << "Nao Bolada\n";
     return 0;
 }
 
