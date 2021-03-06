9424db6a610b08617da34d3c1fa1b7f488ec8678
= <tgomota1@gmail.com>
2021-03-05 22:09:39 -0300

    training cf medium problems

--- Codeforces Solutions/a.cpp
+++ Codeforces Solutions/a.cpp
@@ -24,18 +24,61 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int,int> ii;
 //CHECK THE CONSTRAINTS, PLEASE
+map<int,int> freq;
+vector<int> allfreq;
+int fen[200005], n;
+
+void add(int idx, int x){
+  idx++;
+  const int N = (int)allfreq.size();
+  while(idx <= N){
+    fen[idx]+=x;
+    idx+= idx & (-idx);
+  }
+}
+
+int query(int idx){
+  idx++;
+  const int N = (int)allfreq.size();
+  int sum = 0;
+  while(idx > 0){
+    sum += fen[idx];
+    idx -= idx&(-idx);
+  }
+  return sum;
+}
+
+void build(){
+  memset(fen, 0x0, sizeof(fen));
+  for(int i = 0; i < (int)allfreq.size() ; ++i){
+    add(i, allfreq[i]);
+  }
+}
+
 int main(){
     fastio();
     int t;
     cin >> t;
     for(int ti = 1; ti <= t ; ++ti){
-        int n;
         cin >> n;
-        vector<int> v(n), freq(10, 0);
-        for(int& x : v) {
+        vector<int> v(n);
+        for(int& x : v){
           cin >> x;
+          freq[x]++;
         }
 
+        for(auto x : freq){
+          allfreq.push_back(x.second);
+        }
+
+        sort(allfreq.begin(), allfreq.end());
+        build();
+        int ans = oo;
+        const int N = (int)allfreq.size();
+        for(int c = 1; c <= allfreq.back() ;  ++c){
+          int pos = upper_bound(allfreq.begin(), allfreq.end(), i) - allfreq.begin();
+          ans = min(ans, query(N-1)-query(pos-1));
+        }
     }
     return 0;
 }
